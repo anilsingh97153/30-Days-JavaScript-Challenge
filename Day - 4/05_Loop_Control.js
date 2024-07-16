dad41ad8b690633c02797403fadde8a40859dbb9
anilsingh97153 <anilsuyal97153@gmail.com>
2024-07-16 08:38:34 +0530

    Day - 4 challenge is completed.

---
+++ Day - 4/05_Loop_Control.js
@@ -0,0 +1,16 @@
+// Task 8: Use of 'continue' keyword
+
+for(let i = 1; i<= 10; i++) {
+    if(i==5) {
+        continue;
+    }
+    console.log(i);
+}
+
+// Task 9: Use of 'break' keyword
+for(let i = 1; i<= 10; i++) {
+    if(i==7) {
+        break;
+    }
+    console.log(i);
+}
